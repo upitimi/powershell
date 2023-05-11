f576416c9b531d00b1474f17959ba88cefc9289a
upitimi <116879823+upitimi@users.noreply.github.com>
2023-05-11 10:19:55 +0200

    Add files via upload

---
+++ buscar archivos por tamaño en el equipo.ps1
@@ -0,0 +1,21 @@
+﻿# Script de PowerShell para buscar los archivos más grandes en todo el ordenador con permisos de administrador
+
+# Especificar la ruta del directorio base
+$baseDir = "C:\"
+
+# Especificar el tamaño mínimo del archivo (en MB)
+$minSize = 100
+
+# Buscar todos los archivos en el directorio base y subdirectorios con permisos de administrador
+$files = Get-ChildItem -Path $baseDir -Recurse -File -Force
+
+# Filtrar los archivos por tamaño
+$largeFiles = $files | Where-Object {$_.Length / 1MB -ge $minSize}
+
+# Ordenar los archivos por tamaño descendente
+$largeFiles = $largeFiles | Sort-Object -Property Length -Descending
+
+# Mostrar los archivos más grandes en la pantalla
+foreach ($file in $largeFiles) {
+    Write-Host "$($file.FullName) - Tamaño: $($file.Length / 1MB) MB"
+}
---
+++ equipo en suspension.ps1
@@ -0,0 +1,15 @@
+﻿# Script de PowerShell para poner el sistema en suspensión
+# Se requieren permisos de administrador para ejecutar este script
+
+# Establecer el tiempo de espera para la suspensión (en segundos)
+$timeout = 5
+
+# Mostrar un mensaje de confirmación antes de suspender el sistema
+$msg = New-Object -ComObject WScript.Shell
+$confirm = $msg.Popup("¿Estás seguro de que deseas suspender el sistema?", $timeout, "Suspensión del sistema", 4)
+
+# Si se hace clic en "Aceptar" en la ventana de confirmación, suspender el sistema
+if ($confirm -eq 6) {
+    Start-Sleep -Seconds $timeout
+    [System.Windows.Forms.Application]::SetSuspendState("Suspend", $false, $false)
+}
